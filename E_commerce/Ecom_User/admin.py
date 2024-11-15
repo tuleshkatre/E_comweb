31b6b9b543992e40f14e7b793b63be2760077ca1
Tulesh <tuleshkatre6@gmail.com>
2024-11-15 13:27:51 +0530

    add space in dev branch

--- E_commerce/Ecom_User/admin.py
+++ E_commerce/Ecom_User/admin.py
@@ -5,24 +5,33 @@ from .models import Product, Cart, CartItem, Order, OrderItem, Address
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'vendor', 'category', 'price', 'stock_quantity')
 
+
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ('id', 'customer')
+
 
 @admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ('cart', 'product', 'quantity')
 
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'customer', 'order_date', 'status', 'total_amount')
 
+
+
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ('order', 'product', 'quantity', 'price')
+
+
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
     list_display = ('customer', 'street_address', 'city', 'state', 'zip_code', 'country')
+
+
 
 
 
